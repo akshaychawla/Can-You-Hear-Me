6100e0ff11a1228359d4fba9c3fcd8c5230bc82e
Priyam Tejaswin <priyamtejaswin@gmail.com>
2018-05-27 15:42:39 +0530

    Using data_generator for training.  Changes to be committed: 	modified:   train.py

--- train.py
+++ train.py
@@ -1,57 +1,52 @@
 """
-Script to train the model on Tensorflow hot word detection dataset 
-""" 
-import numpy as np 
-from model import resemul 
-from keras.models import Model 
-from keras.layers import Input 
-from keras import optimizers, callbacks 
-from keras import utils 
+Script to train the model on Tensorflow hot word detection dataset
+"""
 
-# network arch 
-input_tensor = Input(shape=(16000,)) 
+from __future__ import print_function
+import numpy as np
+from model import resemul
+from keras.models import Model
+from keras.layers import Input
+from keras import optimizers, callbacks
+from keras import utils
+from utils import data_generator
+
+# network arch
+input_tensor = Input(shape=(16000,))
 output_tensor = resemul(
-                        input_tensor, block_type="rese", init_features=128,
+                        input_tensor, block_type="basic", init_features=128,
                         amplifying_ratio=16, drop_rate=0.5, weight_decay=0.0,
                         num_classes=30
-                    ) 
-model = Model(input_tensor, output_tensor) 
-print(model.summary()) 
-sgd = optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True) 
-model.compile(sgd, "categorical_crossentropy") 
-print("..Model Compiled") 
+                    )
+model = Model(input_tensor, output_tensor)
+print(model.summary())
+sgd = optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)
+model.compile(sgd, "categorical_crossentropy")
+print("..Model Compiled")
 
-# callbacks 
+# callbacks
 checkpoint = callbacks.ModelCheckpoint(
                     filepath="./checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5",
-                    save_weights_only=True, 
+                    save_weights_only=True,
                     verbose=1
                 )
 lrschedule = callbacks.ReduceLROnPlateau(
-                    monitor="val_loss", 
-                    factor=0.2, 
-                    patience=3, 
-                    verbose=1, 
+                    monitor="val_loss",
+                    factor=0.2,
+                    patience=3,
+                    verbose=1,
                     min_lr=1e-07
                 )
 tboard     = callbacks.TensorBoard(log_dir="./logs/")
 
-# Train the model 
-# dummy data 
-x_train, x_valid = np.random.randn(20, 16000), np.random.randn(5, 16000) 
-y_train, y_valid = np.random.randint(0, 30, size=(20,)), np.random.randint(0, 30, size=(5,))
-y_train, y_valid = utils.to_categorical(y_train, num_classes=30), utils.to_categorical(y_valid, num_classes=30) 
+# Train the model
+dgen = data_generator("/Users/tejaswin.p/Downloads/audio-experiments/speech_commands_v0.01/training.h5py")
 
-history = model.fit(
-                    x = x_train, 
-                    y = y_train, 
-                    batch_size=2, 
-                    epochs=10, 
-                    callbacks=[checkpoint, lrschedule, tboard],
-                    validation_data = (x_valid, y_valid)
-                ) 
+history = model.fit_generator(
+                    dgen,
+                    steps_per_epoch=1597,
+                    epochs=10,
+                    callbacks=[checkpoint, tboard]
+                )
 
 import ipdb; ipdb.set_trace()
-
-
-
