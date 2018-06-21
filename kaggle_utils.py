d67c0ffb2592eb38f2f6fda6fefbfb970fad28b1
Priyam Tejaswin <priyamtejaswin@gmail.com>
2018-06-21 23:29:52 +0530

    Updated name convention of `silence` files. `temp_path = os.path.join(silence_dir, fname[:2]+"%d_nohash_%d.wav"%(count, count))` This ensures a fair 'probabilistic' split.
     Changes to be committed:
    	modified:   kaggle_utils.py

--- kaggle_utils.py
+++ kaggle_utils.py
@@ -4,6 +4,8 @@ Utility codes for data, eval, plotting, etc.
 
 from __future__ import print_function
 import os
+import re
+import hashlib
 import sys
 import librosa
 import scipy
@@ -176,6 +178,55 @@ def create_data_hdf5(root):
     print("\nDone.")
 
 
+MAX_NUM_WAVS_PER_CLASS = 2**27 - 1  # ~134M
+def which_set(filename, validation_percentage, testing_percentage):
+    """Determines which data partition the file should belong to.
+
+    We want to keep files in the same training, validation, or testing sets even
+    if new ones are added over time. This makes it less likely that testing
+    samples will accidentally be reused in training when long runs are restarted
+    for example. To keep this stability, a hash of the filename is taken and used
+    to determine which set it should belong to. This determination only depends on
+    the name and the set proportions, so it won't change as other files are added.
+
+    It's also useful to associate particular files as related (for example words
+    spoken by the same person), so anything after '_nohash_' in a filename is
+    ignored for set determination. This ensures that 'bobby_nohash_0.wav' and
+    'bobby_nohash_1.wav' are always in the same set, for example.
+
+    Args:
+      filename: File path of the data sample.
+      validation_percentage: How much of the data set to use for validation.
+      testing_percentage: How much of the data set to use for testing.
+
+    Returns:
+      String, one of 'training', 'validation', or 'testing'.
+    """
+    base_name = os.path.basename(filename)
+    # We want to ignore anything after '_nohash_' in the file name when
+    # deciding which set to put a wav in, so the data set creator has a way of
+    # grouping wavs that are close variations of each other.
+    hash_name = re.sub(r'_nohash_.*$', '', base_name)
+    # This looks a bit magical, but we need to decide whether this file should
+    # go into the training, testing, or validation sets, and we want to keep
+    # existing files in the same set even if more files are subsequently
+    # added.
+    # To do that, we need a stable way of deciding based on just the file name
+    # itself, so we do a hash of that and then use that to generate a
+    # probability value that we use to assign it.
+    hash_name_hashed = hashlib.sha1(hash_name).hexdigest()
+    percentage_hash = ((int(hash_name_hashed, 16) %
+                        (MAX_NUM_WAVS_PER_CLASS + 1)) *
+                       (100.0 / MAX_NUM_WAVS_PER_CLASS))
+    if percentage_hash < validation_percentage:
+      result = 'validation'
+    elif percentage_hash < (testing_percentage + validation_percentage):
+      result = 'testing'
+    else:
+      result = 'training'
+    return result
+
+
 def add_silence(data_root):
     silence_dir = os.path.join(data_root, "silence")
     bnoise_dir = os.path.join(data_root, "_background_noise_")
@@ -215,7 +266,7 @@ def add_silence(data_root):
                     up = alen
 
                 temp_audio = audio[ix:up]
-                temp_path = os.path.join(silence_dir, fname[:2]+"_nohash_%d.wav"%count)
+                temp_path = os.path.join(silence_dir, fname[:2]+"%d_nohash_%d.wav"%(count, count))
 
                 if len(temp_audio) >= 10000:
                     if len(temp_audio) < SRATE:
@@ -226,7 +277,6 @@ def add_silence(data_root):
                     librosa.output.write_wav(temp_path, temp_audio, SRATE)
 
             assert up==alen, "YO! Your indexing limits are not matching!!!"
-
 
 
 if __name__ == '__main__':
@@ -245,4 +295,8 @@ if __name__ == '__main__':
 
     elif what == "silence":
         add_silence(data_root)
+        print("--data splitting--")
+        for fname in os.listdir(os.path.join(data_root, "silence")):
+            print(which_set(fname, 10, 10))
+
         print("\n\nTime taken:", (time.time() - _st)/60, "mins.")
